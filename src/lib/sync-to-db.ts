b400c34679131241616311a8974169031b96b9d7
chuba <chinemelumchubanwene57@gmail.com>
2025-01-13 10:57:42 +0100

    fix: fix orama sync issue

--- src/lib/sync-to-db.ts
+++ src/lib/sync-to-db.ts
@@ -6,76 +6,96 @@ import type { EmailAddress, EmailAttachment, EmailMessage } from "@/types";
 import { Prisma } from "@prisma/client";
 import pLimit from "p-limit";
 import { OramaClient } from "./orama";
-// import { turndown } from "./turndown";
-// import { getEmbeddings } from "./embeddings";
+import { turndown } from "./turndown";
+import { getEmbeddings } from "./embeddings";
 
-export const syncEmailsToDatabase = async (emails: EmailMessage[], accountId: string) => {
+export const syncEmailsToDatabase = async (
+  emails: EmailMessage[],
+  accountId: string,
+) => {
   console.log("Attempting to sync emails to database", emails.length);
-  // const promiseBatchLimit = pLimit(10); //process up to ten emails concurrently
+  const promiseBatchLimit = pLimit(10); //process up to ten emails concurrently
 
-  // const orama = new OramaClient(accountId);
-  // await orama.initialize();
+  const orama = new OramaClient(accountId);
+  await orama.initialize();
 
   try {
+    async function syncToOrama() {
+      await Promise.all(
+        emails.map((email) => {
+          return promiseBatchLimit(async () => {
+            const body = turndown.turndown(
+              email.body ?? email.bodySnippet ?? "",
+            );
+            const embeddings = await getEmbeddings(body);
 
-    // async function syncToOrama() {
-    //   await Promise.all(emails.map(email => {
-    //     return promiseBatchLimit(async () => {
-    //       const body = turndown.turndown(email.body ?? email.bodySnippet ?? "");
-    //       const embeddings = await getEmbeddings(body);
-          
-    //       await orama.insert({
-    //         body,
-    //         subject: email.subject,
-    //         rawBody: email.bodySnippet ?? "",
-    //         from: `${email.from.name} <${email.from.address}>`,
-    //         to: email.to.map((t) => `${t.name} <${t.address}>`),
-    //         sentAt: new Date(email.sentAt).toLocaleString(),
-    //         threadId: email.threadId,
-    //         embeddings
-    //       });
-    //     })
-    //   }))
-    // }
+            await orama.insert({
+              body,
+              subject: email.subject,
+              rawBody: email.bodySnippet ?? "",
+              from: `${email.from.name} <${email.from.address}>`,
+              to: email.to.map((t) => `${t.name} <${t.address}>`),
+              sentAt: new Date(email.sentAt).toLocaleString(),
+              threadId: email.threadId,
+              embeddings,
+            });
+          });
+        }),
+      );
+    }
 
     async function syncToDB() {
-      for (const [ index, email ] of emails.entries()) {
-        console.log(`Email ${email.subject} ${index} ready to upsert`)
+      for (const [index, email] of emails.entries()) {
+        console.log(`Email ${email.subject} ${index} ready to upsert`);
         await upsertEmail(email, accountId, index);
-        console.log(`Email ${email.subject} upserted`)
+        console.log(`Email ${email.subject} upserted`);
       }
     }
 
-    await Promise.all([ syncToDB() ]);
-    // await orama.saveIndex();
-
+    await Promise.all([syncToDB()]);
+    await Promise.all([syncToOrama()]);
+    await orama.saveIndex();
   } catch (error) {
     console.error("Failed to sync emails to database", error);
     throw error;
   }
-}
+};
 
-const upsertEmail = async(email: EmailMessage, accountId: string, index: number) => {
+const upsertEmail = async (
+  email: EmailMessage,
+  accountId: string,
+  index: number,
+) => {
   console.log("upserting email: ", index);
 
   try {
     let emailLabelType: "inbox" | "sent" | "draft" = "inbox";
-    if (email.sysLabels.includes("inbox") || email.sysLabels.includes("important")) {
+    if (
+      email.sysLabels.includes("inbox") ||
+      email.sysLabels.includes("important")
+    ) {
       emailLabelType = "inbox";
     } else if (email.sysLabels.includes("sent")) {
       emailLabelType = "sent";
     } else if (email.sysLabels.includes("draft")) {
-      emailLabelType = 'draft';
+      emailLabelType = "draft";
     }
 
     //1. Upsert EmailAddress Records
     const addressesToUpsert = new Map<string, EmailAddress>();
 
-    for(const address of [email.from, ...email.to, ...email.cc, ...email.bcc, ...email.replyTo]) {
+    for (const address of [
+      email.from,
+      ...email.to,
+      ...email.cc,
+      ...email.bcc,
+      ...email.replyTo,
+    ]) {
       addressesToUpsert.set(address.address, address);
     }
 
-    const upsertedAddresses: (Awaited<ReturnType<typeof upsertEmailAddress>>)[] = [];
+    const upsertedAddresses: Awaited<ReturnType<typeof upsertEmailAddress>>[] =
+      [];
 
     for (const address of addressesToUpsert.values()) {
       const upsertedAddress = await upsertEmailAddress(address, accountId);
@@ -83,20 +103,32 @@ const upsertEmail = async(email: EmailMessage, accountId: string, index: number)
     }
 
     const addressMap = new Map(
-      upsertedAddresses.filter(Boolean).map((address) => [address!.address, address])
-    )
+      upsertedAddresses
+        .filter(Boolean)
+        .map((address) => [address!.address, address]),
+    );
 
     const fromAddress = addressMap.get(email.from.address);
 
     if (!fromAddress) {
-      console.error(`Failed to upsert from-addresses from email ${email.bodySnippet}`);
+      console.error(
+        `Failed to upsert from-addresses from email ${email.bodySnippet}`,
+      );
       return;
     }
 
-    const toAddresses = email.to.map((address) => addressMap.get(address.address)).filter(Boolean);
-    const ccAddresses = email.cc.map((address) => addressMap.get(address.address)).filter(Boolean);
-    const bccAddresses = email.bcc.map((address) => addressMap.get(address.address)).filter(Boolean);
-    const replyToAddresses = email.replyTo.map((address) => addressMap.get(address.address)).filter(Boolean);
+    const toAddresses = email.to
+      .map((address) => addressMap.get(address.address))
+      .filter(Boolean);
+    const ccAddresses = email.cc
+      .map((address) => addressMap.get(address.address))
+      .filter(Boolean);
+    const bccAddresses = email.bcc
+      .map((address) => addressMap.get(address.address))
+      .filter(Boolean);
+    const replyToAddresses = email.replyTo
+      .map((address) => addressMap.get(address.address))
+      .filter(Boolean);
 
     //2. Upsert Thread
     const thread = await db.thread.upsert({
@@ -203,7 +235,7 @@ const upsertEmail = async(email: EmailMessage, accountId: string, index: number)
 
     const threadEmails = await db.email.findMany({
       where: { threadId: thread.id },
-      orderBy: { receivedAt: "asc" }
+      orderBy: { receivedAt: "asc" },
     });
 
     let threadFolderType = "sent";
@@ -212,7 +244,7 @@ const upsertEmail = async(email: EmailMessage, accountId: string, index: number)
         threadFolderType = "inbox";
         break; //If any thread email is labelled as inbox then the whole thread is in inbox
       } else if (threadEmail.emailLabel === "draft") {
-        threadFolderType = "draft" //Set to draft and continue checking for inbox
+        threadFolderType = "draft"; //Set to draft and continue checking for inbox
       }
     }
 
@@ -221,8 +253,8 @@ const upsertEmail = async(email: EmailMessage, accountId: string, index: number)
       data: {
         draftStatus: threadFolderType === "draft",
         inboxStatus: threadFolderType === "inbox",
-        sentStatus: threadFolderType === "sent"
-      }
+        sentStatus: threadFolderType === "sent",
+      },
     });
 
     //4. Upsert Attachemnts
@@ -236,32 +268,44 @@ const upsertEmail = async(email: EmailMessage, accountId: string, index: number)
       console.error(`Unknown error for email ${email.id}: ${error as any}`);
     }
   }
-}
+};
 
 const upsertEmailAddress = async (address: EmailAddress, accountId: string) => {
   try {
     const existingAddress = await db.emailAddress.findUnique({
-      where: { accountId_address: { accountId: accountId, address: address.address ?? "" } }
-    })
+      where: {
+        accountId_address: {
+          accountId: accountId,
+          address: address.address ?? "",
+        },
+      },
+    });
 
     if (existingAddress) {
       return await db.emailAddress.update({
         where: { id: existingAddress.id },
-        data: { name: address.name, raw: address.raw }
+        data: { name: address.name, raw: address.raw },
       });
     } else {
       return await db.emailAddress.create({
-        data: { address: address.address, name: address.name, raw: address.raw, accountId }
-      })
+        data: {
+          address: address.address,
+          name: address.name,
+          raw: address.raw,
+          accountId,
+        },
+      });
     }
-
   } catch (error) {
     console.error("Failed to upset email address", error);
     return null;
   }
-}
+};
 
-const upsertEmailAttachement = async (emailId: string, attachment: EmailAttachment) => { 
+const upsertEmailAttachement = async (
+  emailId: string,
+  attachment: EmailAttachment,
+) => {
   try {
     await db.emailAttachment.upsert({
       where: { id: attachment.id ?? "" },
@@ -287,6 +331,8 @@ const upsertEmailAttachement = async (emailId: string, attachment: EmailAttachme
       },
     });
   } catch (error) {
-    console.error(`Failed to upser attachment for email ${emailId}: ${error as any}`)
+    console.error(
+      `Failed to upser attachment for email ${emailId}: ${error as any}`,
+    );
   }
-}
+};
