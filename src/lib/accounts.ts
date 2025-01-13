401dda8a30a8b5427741939afa88b104902422a7
chuba <chinemelumchubanwene57@gmail.com>
2025-01-13 13:17:06 +0100

    fix: reduce email sync time length

--- src/lib/accounts.ts
+++ src/lib/accounts.ts
@@ -44,7 +44,7 @@ export class Account {
           Authorization: `Bearer ${this.token}`,
         },
         params: {
-          daysWithin: 30,
+          daysWithin: 10,
           bodyType: "html",
         },
       },
